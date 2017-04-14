3786dcd8ddaaa75ee3f2079396b6e7d876a4c2a8
Ryota Kaji <ryota.kaji531@gmail.com>
2017-04-14 04:51:02 +0000

    DRY, validation, add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
@@ -51,6 +53,10 @@ class TasksController < ApplicationController
   
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
+  end
+  
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
